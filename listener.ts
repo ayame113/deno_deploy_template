f486af5a991be419cf02e280c714277240263403
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-10-17 23:20:42 +0900

    deault listener

--- listener.ts
+++ listener.ts
@@ -1,3 +1,5 @@
+import { contentType } from "https://deno.land/x/media_types@v2.10.2/mod.ts";
+
 if (!Deno.env.get("DENO_DEPLOYMENT_ID")) {
   await import("https://deno.land/x/dotenv@v3.0.0/load.ts");
 }
@@ -10,6 +12,15 @@ interface Listener {
 }
 
 export const listeners: Listener[] = [{
-  pattern: new URLPattern("..."),
-  handler: ({ request, url }) => new Response(),
+  pattern: new URLPattern("http{s}?://*/"),
+  handler: ({ request, url }) =>
+    new Response(
+      'Hello World from <a href="https://github.com/ayame113/deno_deploy_template">ayame113/deno_deploy_template</a>!',
+      {
+        headers: {
+          "Content-Type": contentType("html") ??
+            "text/plain",
+        },
+      },
+    ),
 }];
