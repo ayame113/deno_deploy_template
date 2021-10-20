4b4674b17f2507c95908c14a34244a6e5a801383
GitHub <noreply@github.com>
2021-10-20 05:54:47 +0000

    chore(deps): update deno dependencies

--- serve_test.ts
+++ serve_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.111.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.112.0/testing/asserts.ts";
 
 import { serve } from "./serve.ts";
 
